6fb43547f85b8098c67a9d9636cd056d13d8070c
jeayoung98 <jjy0843@gmail.com>
2026-04-16 10:14:18 +0900

    init

---
+++ src/main/java/com/example/ssoj/SsojApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssoj/SsojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
